e0613b5731f963b3a6cc52ad1c926128bedf9b95
Juang22 <57643933+Juang22@users.noreply.github.com>
2022-10-13 18:07:29 -0300

    new changes

--- back/app.js
+++ back/app.js
@@ -4,20 +4,18 @@ const mongoose = require('mongoose')
 const api = require('./config/api')
 require('dotenv').config();
 
-app.use( (req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
+// app.use( (req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//   res.setHeader('Access-Control-Allow-Credentials', true);
+//   next();
+// });
 
 const port = process.env.APP_PORT;
 const uriMDB = process.env.MONGODB_URI;
 //connect mongoDB database
-mongoose.connect(uriMDB).then(() => {
-    console.log('connected to MongoDB Atlas')
-}).catch((error) => console.log(error) )
+
 
 
 app.use(
@@ -31,4 +29,6 @@ app.use('/api/v1', api.v1)
 app.listen(port,(error) => {
     console.log(`listening in port ${port} `)
 })
- 
+mongoose.connect(uriMDB).then(() => {
+  console.log('connected to MongoDB Atlas')
+}).catch((error) => console.log(error) )
--- back/routes/route.js
+++ back/routes/route.js
@@ -1,38 +1,45 @@
 const express = require('express');
 const routes = express.Router()
+const PlatoSchema = require('../model/platos')
 // const { login, servicios, register } = require('./operations')
 
 
 
-routes.get('/players',(req,res) => {
-    servicios(pool,req,result => {
-        res.json(result)
-    })
+routes.post('/platos',(req,res) => {
+    // servicios(pool,req,result => {
+    //     res.json(result)
+    // })
+    console.log(req.body)
+    const plato = PlatoSchema(req.body);
+    console.log(req.body)
+    plato.save()
+        .then(data => res.json(data))
+        .catch(error => res.json({message:error}))
 })
 
-routes.get('/teams',(req,res) => {
-    servicios(pool,req,result => {
-        res.json(result)
-    })
-})
+// routes.get('/teams',(req,res) => {
+//     servicios(pool,req,result => {
+//         res.json(result)
+//     })
+// })
 
-routes.post('/players',(req,res) => {
-    login(pool,req,result => {
-        res.json(result)
-    })
-})
+// routes.post('/players',(req,res) => {
+//     login(pool,req,result => {
+//         res.json(result)
+//     })
+// })
 
-routes.post('/register',(req,res) => {
-    register(pool,req,result => {
-        res.json(result)
-    })
-})
+// routes.post('/register',(req,res) => {
+//     register(pool,req,result => {
+//         res.json(result)
+//     })
+// })
 
-routes.put('/players  ',(req,res) => {
-    login(pool,req,result => {
-        res.json(result)
-    })
-})
+// routes.put('/players  ',(req,res) => {
+//     login(pool,req,result => {
+//         res.json(result)
+//     })
+// })
 
 module.exports = routes;
     
